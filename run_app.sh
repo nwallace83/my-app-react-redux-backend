3b6f36642cf645ecfd6a4298b581b90ca789ae23
Nathan Wallace <nwallace6988@gmail.com>
2021-07-11 12:04:35 -0600

    set NODE_ENV in container

--- run_app.sh
+++ run_app.sh
@@ -4,4 +4,4 @@ git pull
 npm install
 docker rm -f my-web-app-backend
 docker build --rm -f Dockerfile.prod -t my-web-app-backend .
-docker run -d --network my-net --hostname backend1 --restart=always --name my-web-app-backend my-web-app-backend
+docker run -d --network my-net --hostname backend1 --env NODE_ENV=production --restart=always --name my-web-app-backend my-web-app-backend
