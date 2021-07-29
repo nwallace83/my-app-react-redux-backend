125779747f7efd6ee2d6a0f30823318a9f732a0c
Nathan Wallace <nwallace6988@gmail.com>
2021-07-28 22:43:54 -0600

    Refactor admin middleware

--- src/middlware/adminOnly.js
+++ src/middlware/adminOnly.js
@@ -1,17 +1,27 @@
 const authService = require('../services/auth-service')
 
-const adminOnly = async (req,res,next) => {
+const adminOnly = (req,res,next) => {
     if (!req.cookies.authorization) {
         res.status(401).send("Invalid Authorization")
     } else {
-        const user = await authService.getUserFromToken(req.cookies.authorization)
-        if (user && user.role === "ADMIN") {
-            console.log("Valid Admin: " + user.userName + " " + user.role)
-            next()
-        } else {
-            console.error("InValid Admin: " + user.userName + " " + user.role)
-            res.status(401).send("User is not an admin")
-        }
+        authService.getUserFromToken(req.cookies.authorization).then(tokenUser => {
+            if (tokenUser) {
+                authService.fetchUser(tokenUser.userName).then(user => {
+                    if (user && user.role === "ADMIN") {
+                        console.log("Valid Admin: " + user.userName + " " + user.role)
+                        next()
+                    } else {
+                        console.error("InValid Admin: " + user)
+                        res.status(401).send("User is not an admin")
+                    }
+                })
+            } else {
+                res.sendStatus(401)
+            }
+        }).catch(err => {
+            console.log(err)
+            res.status(401).send("An Error Occured")
+        })
     }
 }
 
